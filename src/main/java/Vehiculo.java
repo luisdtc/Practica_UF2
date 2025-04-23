9dbb4a7c2e187ca387e4b2978ce73ba3d12af963
ldeltoro <luistc00@gmail.com>
2025-04-23 23:46:16 +0200

    feat: se crea el metodo acelerar de la clase vehiculo

--- src/main/java/Vehiculo.java
+++ src/main/java/Vehiculo.java
@@ -3,7 +3,7 @@ public class Vehiculo {
     private final String marca;
     private final String modelo;
     private final int velocidadMaxima;
-    private final int velocidadActual;
+    private int velocidadActual;
 
     public Vehiculo(String marca, String modelo, int velocidadMaxima) {
         this.marca = marca;
@@ -12,8 +12,19 @@ public class Vehiculo {
         this.velocidadActual = 0;
     }
 
-    public void acelerar(int incremento){
+    public void acelerar(int incremento) {
+        if (incremento <= 0) {
+            System.out.println("Aceleración no válida: debe ser un número entero positivo.");
+            return;
+        }
+
+        velocidadActual += incremento;
+
+        if (velocidadActual > velocidadMaxima) {
+            velocidadActual = velocidadMaxima;
+        }
     }
+
 
     public String getMarca() {
         return marca;
