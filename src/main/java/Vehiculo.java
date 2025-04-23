9c3cf99ac0b1d445a9d0990b47e2e10fe979979c
ldeltoro <luistc00@gmail.com>
2025-04-23 20:55:41 +0200

    feat: se añaden getter de clase vehiculo

--- src/main/java/Vehiculo.java
+++ src/main/java/Vehiculo.java
@@ -1,5 +1,27 @@
 public class Vehiculo {
+
+    private String marca;
+    private String modelo;
+    private int velocidadMaxima;
+    private int velocidadActual;
+
     public Vehiculo(String marca, String modelo, int velocidadMaxima) {
         System.out.println("constructor vehiculo");
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    public int getVelocidadActual() {
+        return velocidadActual;
     }
 }
