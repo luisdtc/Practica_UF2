71487ccbeae1c1004d026729690f422d2e26248c
mardrrr <75481071+mardrrr@users.noreply.github.com>
2025-04-25 01:47:02 +0200

    Update VehiculoPararTest.java retunr to original

--- src/test/java/VehiculoPararTest.java
+++ src/test/java/VehiculoPararTest.java
@@ -5,33 +5,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class VehiculoPararTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+	@BeforeEach
+	void setUp() {
+	}
 
-   // @Test
-   // public void testPararTC_PA_01() {
+	@Test
+	public void testPararTC_PA_01() {
 
-     //   Vehiculo vehiculo = new Vehiculo("Toyota", "Corolla", 100);
+    	Vehiculo vehiculo = new Vehiculo("Toyota", "Corolla", 100);
 
-        // Acelerar a 80
-       // vehiculo.acelerar(80);
+    	// Acelerar a 80
+    	vehiculo.acelerar(80);
 
-        //vehiculo.parar();
+    	vehiculo.parar();
 
-        // Verificar que la velocidad es 0
-        // assertEquals(0, vehiculo.getVelocidadActual(), "La velocidad debe ser 0 después de parar.");
-  //  }
-    	@Test
+    	// Verificar que la velocidad es 0
+    	assertEquals(0, vehiculo.getVelocidadActual(), "La velocidad debe ser 0 después de parar.");
+	}
+
+	@Test
 	public void testPararTC_PA_02() {
-        1=1
-    	//Vehiculo vehiculo = new Vehiculo("Toyota", "Corolla", 100);
 
-    	//vehiculo.parar();
+    	Vehiculo vehiculo = new Vehiculo("Toyota", "Corolla", 100);
+
+    	vehiculo.parar();
 
     	// Verificar que la velocidad sigue siendo 0
-    	//assertEquals(0, vehiculo.getVelocidadActual(), "La velocidad debe permanecer en 0 después de llamar a parar.");
+    	assertEquals(0, vehiculo.getVelocidadActual(), "La velocidad debe permanecer en 0 después de llamar a parar.");
 	}
 }
 
- }
