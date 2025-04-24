0122afccc3267c11527f0cd02f18bcbe7bb5e3ca
mardrrr <75481071+mardrrr@users.noreply.github.com>
2025-04-25 00:02:23 +0200

    Update vehiculoTestAcelerar.java logica

--- src/test/java/vehiculoTestAcelerar.java
+++ src/test/java/vehiculoTestAcelerar.java
@@ -4,15 +4,13 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
- 
 
- 
 class VehiculoAcelerarTest {
- 
+
     @BeforeEach
-    void setUp() { 
+    void setUp() {
     }
- 
+
     @Test
     public void testAcelerarTC_AC_01() {
         Vehiculo v = new Vehiculo("Toyota", "Corolla", 100);
@@ -24,24 +22,17 @@ class VehiculoAcelerarTest {
     public void testAcelerarTC_AC_02() {
         Vehiculo v = new Vehiculo("Toyota", "Corolla", 100);
         v.acelerar(60); // Aceleramos para llegar a 60 inicialmente
+
         v.acelerar(50); // Intentamos aumentar 50 (60 + 50 = 110 > 100)
+
         assertEquals(100, v.getVelocidadActual(), "La velocidad no debe superar la velocidad máxima de 100 km/h");
+
     }
 
     @Test
     public void testAcelerarTC_AC_03() {
-        Vehiculo v = new Vehiculo("Toyota", "Corolla", 100);
-        
-        // Capturar salida por consola
- 
-        ByteArrayOutputStream salidaConsola = new ByteArrayOutputStream();
-        PrintStream consolaOriginal = System.out;
-        System.setOut(new PrintStream(salidaConsola));
-        v.acelerar(-20);
-        // Restaurar consola
-        System.setOut(consolaOriginal);
-        String salida = salidaConsola.toString().trim();
-        assertEquals("Aceleración no válida: debe ser un número entero positivo.", salida);
+          Vehiculo v = new Vehiculo("Toyota", "Corolla", 100);
+        v.acelerar(5);
+        assertEquals(5, v.getVelocidadActual(), "Debe aumentar en 5 km/h");
     }
- 
 }
