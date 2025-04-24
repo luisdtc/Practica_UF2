bf0c9ebe06f09a951bc1cc19330e697d73d68c9e
mardrrr <75481071+mardrrr@users.noreply.github.com>
2025-04-24 22:23:32 +0200

    Update VehiculoFrenarTest.java preuba1

--- src/test/java/VehiculoFrenarTest.java
+++ src/test/java/VehiculoFrenarTest.java
@@ -4,33 +4,27 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.*;
 
-class VehiculoFrenarTest {
-
-    @BeforeEach
-    void setUp() {
-    }
+public class VehiculoFrenarTest {
 
     @Test
-    public void testFrenarTC_FR_01() {
-        Vehiculo vehiculo = new Vehiculo("Seat", "Ibiza", 150);
+    void frenar_decrementoPositivo() {
+        Vehiculo vehiculo = new Vehiculo("Seat", "Ibiza", 100);
         vehiculo.acelerar(50); // Necesitamos acelerar primero para poder frenar
         vehiculo.frenar(10);
         assertEquals(40, vehiculo.getVelocidadActual());
     }
 
     @Test
-    public void testFrenarTC_FR_02() {
-        Vehiculo vehiculo = new Vehiculo("Seat", "Ibiza", 150);
+    void frenar_hastaParar() {
+        Vehiculo vehiculo = new Vehiculo("Seat", "Ibiza", 100);
         vehiculo.acelerar(30);
         vehiculo.frenar(50); // Intentamos frenar más de lo que vamos
         assertEquals(0, vehiculo.getVelocidadActual());
     }
 
-
     @Test
-    public void testFrenarTC_FR_03() {
-        Vehiculo v = new Vehiculo("Seat", "Ibiza", 150);
-        Vehiculo vehiculo = new Vehiculo("MarcaX", "ModeloY", 100);
+    void frenar_decrementoNegativoNoCambiaVelocidad() {
+        Vehiculo vehiculo = new Vehiculo("Seat", "Ibiza", 100);
         vehiculo.acelerar(20);
         vehiculo.frenar(-5);
         assertEquals(20, vehiculo.getVelocidadActual());
