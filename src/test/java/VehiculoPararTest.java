b6a50bdf642c307a41cffccb3ccb5ac89d49bc88
mardrrr <75481071+mardrrr@users.noreply.github.com>
2025-04-24 21:30:19 +0200

    Update VehiculoPararTest.java cambio

--- src/test/java/VehiculoPararTest.java
+++ src/test/java/VehiculoPararTest.java
@@ -1,29 +1,26 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class VehiculoPararTest {
 
+    private Vehiculo vehiculo;
+
     @BeforeEach
     void setUp() {
+        vehiculo = new Vehiculo("Toyota", "Corolla", 100);
     }
 
     @Test
     void testPararTC_PA_01() {
-        Vehiculo vehiculo = new Vehiculo("Toyota", "Corolla", 100);
         vehiculo.acelerar(80);
         vehiculo.parar();
         // Verificar que la velocidad es 0
         assertEquals(0, vehiculo.getVelocidadActual());
     }
-    @BeforeEach
-    void setUp() {
-    }
-    
+
     @Test
     void testPararTC_PA_02() {
-        Vehiculo vehiculo = new Vehiculo("Toyota", "Corolla", 100);
         vehiculo.parar();
         // Verificar que la velocidad sigue siendo 0
         assertEquals(0, vehiculo.getVelocidadActual());
