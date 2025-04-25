b619770d90a385019ff1f82f1aa3710cede74638
mardrrr <75481071+mardrrr@users.noreply.github.com>
2025-04-25 02:10:34 +0200

    Update VehiculoFrenarTest.java out public

--- src/test/java/VehiculoFrenarTest.java
+++ src/test/java/VehiculoFrenarTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class VehiculoFrenarTest {
+    class VehiculoFrenarTest {
 
     private Vehiculo vehiculo;
 
